a51838b36afd555fb4f8f0ec02a02cb8ff09980c
Sebastian Mandal <sebastian.mandal@icloud.com>
2021-07-26 01:20:06 +0200

    Changed example directories for readability

--- src/core/pages.ts
+++ src/core/pages.ts
@@ -1,13 +1,13 @@
 import { PageType } from "./types";
 
 // Index
-import index from "../pages/examples/home";
+import index from "../examples/pages/home";
 export const IndexPage: PageType = index;
 
 // About
-import about from "../pages/examples/about";
+import about from "../examples/pages/about";
 export const AboutPage: PageType = about;
 
 // API example
-import api_example from "../pages/examples/api_example";
+import api_example from "../examples/api/api_example";
 export const API_Example: PageType = api_example;
