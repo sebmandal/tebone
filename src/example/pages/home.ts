90b5c3e25c57c3dc821874d9f8a4db408b696923
Sebastian Mandal <sebastian.mandal@icloud.com>
2021-08-03 02:12:12 +0200

    Update home.ts

--- src/example/pages/home.ts
+++ src/example/pages/home.ts
@@ -13,7 +13,7 @@ const script = (req: Express.Request, res: Express.Response) => {
 export default class Home extends Route {
 	/**
 	 * super()
-	 * parameter 1: the URL path (/api/employees)
+	 * parameter 1: the URL path (/)
 	 * parameter 2: the Express routing method (GET)
 	 * parameter 3: the Express middleware/handler function (script)
 	 **/
