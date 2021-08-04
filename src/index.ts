b0cf4dc82f69f14504e2cf31feb10704a855e3d8
Sebastian Mandal <sebastian.mandal@icloud.com>
2021-08-04 19:33:16 +0200

    minor change, fixed clarity view engine README

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import path from "path";
 // Set up Express app (views, static files, etc.)
 const app = express();
 app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); // change ejs to whatever you want
 app.use(express.static("./public"));
 
 // Automatically configure Express routes
